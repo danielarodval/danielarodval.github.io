2613c23593bc6bea515728f4f609e8b54d474598
danielarodval <49764316+danielarodval@users.noreply.github.com>
2024-01-07 03:21:51 -0500

    git merge --strategy-option theirs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
     reactStrictMode: true,
     assetPrefix: process.env.GITHUB_PAGES ? '/danielarodval.github.io' : '',
     output: 'standalone'
-=======
-    reactStrictMode: true
->>>>>>> 36a90fc6 (git commit -m "changing website to react")
 }
